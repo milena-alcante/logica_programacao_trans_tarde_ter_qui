fbcf42675bd448dbf763ac309988475d467bd69b
MILENA ALCANTE LISBOA ACIOLE <milena.alaciole@senacsp.edu.br>
2025-01-14 15:23:43 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ola´mundo! Sou Milena");
+Console.WriteLine("Estou aprendendo lógica de programação");
---
+++ ex03/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Meu nome é Milena!");
+Console.WriteLine("Estou estudando no Senac Lago Treze");
