8741b317a381e326bf85b7442e87dbcda0755397
MILENA ALCANTE LISBOA ACIOLE <milena.alaciole@senacsp.edu.br>
2025-01-23 14:42:28 -0300

    wx

---
+++ ex9/Program.cs
@@ -0,0 +1,26 @@
+﻿// Exibir na tela os números de 0 até 50
+
+// 01 - Apenas While
+int i = 0;
+
+while (i <= 50)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+int a = 0;
+
+// 02 - Do While
+do
+{
+    Console.WriteLine(a);
+    a++;
+} while (a <= 50);
+
+// 03 - Polo for
+
+for (int u = 1; u < 51; u++)
+{  
+    Console.WriteLine(i);
+}  
