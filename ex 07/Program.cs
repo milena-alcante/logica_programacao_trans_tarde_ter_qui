4b5eec7b86ebcc5dd7b9c0fbbbf4c0cb52fa7011
MILENA ALCANTE LISBOA ACIOLE <milena.alaciole@senacsp.edu.br>
2025-01-23 14:44:11 -0300

    jen

---
+++ ex 07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Descrição: Crie um programa que gera a tabuada de um numero escolhido pelo usuario.
+//Do While
+
+Console.WriteLine("Digite um número para ver a tabuada: ");
+int num = int.Parse(Console.ReadLine());
+int contador = 1;
+
+do
+{
+    Console.WriteLine($"{num} x {contador} = {num * contador}" );
+    contador++;
+
+}
+while (contador <= 10);
