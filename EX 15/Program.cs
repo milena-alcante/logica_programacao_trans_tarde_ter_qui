e5f6bd45a170c434e5124b59ea5554c7b20d9638
MILENA ALCANTE LISBOA ACIOLE <milena.alaciole@senacsp.edu.br>
2025-01-30 15:27:06 -0300

    ex

---
+++ EX 15/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+
+Desafio: Criar um programa que receba 6 nome utilizando arrays. Ao final o programa deve exibir todos os nome digitados
+
+*/
+
+string[] nomes = new string[6];
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Digite o nome:");
+    nomes[i] = Console.ReadLine();
+}
+Console.Clear();
+
+for (int i = 0;i < nomes.Length;i++)
+{
+    Console.WriteLine($"Nomes: {nomes[i]}");
+}
