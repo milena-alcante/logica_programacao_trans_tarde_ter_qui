fb6b18f2c606889a5041e07b2973a239e3215063
MILENA ALCANTE LISBOA ACIOLE <milena.alaciole@senacsp.edu.br>
2025-01-30 15:25:47 -0300

    ex

---
+++ EX12/Program.cs
@@ -0,0 +1,21 @@
+﻿//int num1 = 1;
+//int num2 = 2;
+//int num3 = 3;
+//int num4 = 4;
+//int num5 = 5;
+
+//Console.WriteLine($"{num1}, {num2}, {num3}, {num4}, {num5}");
+
+int [] num = { 1, 2, 3, 4, 5 };
+//Console.WriteLine(num[0]);
+//Console.WriteLine(num[1]);
+//Console.WriteLine(num[2]);
+//Console.WriteLine(num{3]);
+//Console.WriteLine(num[4]);
+
+/* Array utilizando for*/
+
+for (int i = 0; i < num.Length; i++)
+{
+   Console.WriteLine($"O valoe que está na posição é: {num [i]} " );
+}
